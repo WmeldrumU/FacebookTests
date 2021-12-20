f2a252cd33c6d5acb857a4dc3e5e47db280f76b7
WmeldrumU <42564258+WmeldrumU@users.noreply.github.com>
2021-12-20 13:25:15 -0700

    finished Kaitenzushi

--- Kaitenzushi/main.cpp
+++ Kaitenzushi/main.cpp
@@ -18,6 +18,27 @@ int main() {
 
 
 int getMaximumEatenDishCount(int N, vector<int> D, int K) {
-
-  return 0;
+  // Write your code here
+  deque<int> dq;
+  deque<int>::iterator it;
+  int dishes_eaten = 0;
+  set<int> lookupSet;
+  
+  for (int i = 0; i < N; i++) { //loop through Conveyor Belt (D)
+    int item = D[i];
+    
+    
+    if (lookupSet.count(item) == 0) { //if not in queue
+      if (dq.size() == K)  {
+        lookupSet.erase(dq.back());
+        dq.pop_back();
+      }
+      
+      
+      dq.push_front(item);
+      lookupSet.insert(item);
+      dishes_eaten++;
+    }
+  }
+  return dishes_eaten;
 }
