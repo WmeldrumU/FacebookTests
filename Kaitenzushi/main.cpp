c03dc9444063195435d5b85a33e257718cf37fba
WmeldrumU <42564258+WmeldrumU@users.noreply.github.com>
2021-12-20 12:34:30 -0700

    Kaitenzushi setup

---
+++ Kaitenzushi/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//forward declarations
+int getMaximumEatenDishCount(int N, vector<int> D, int K);
+
+
+int main() {
+    int N = 0;
+    vector<int> D = {};
+    int K = 0;
+
+    cout << getMaximumEatenDishCount(N, D, K) << endl;
+
+    return 0;
+}
+
+
+int getMaximumEatenDishCount(int N, vector<int> D, int K) {
+
+  return 0;
+}
